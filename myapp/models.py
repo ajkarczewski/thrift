ff63fb1dd3c22994b4cebf1bd46cbb578b5f9d5f
AJKAR <karczewskiamanda@gmail.com>
2022-04-13 12:39:45 -0500

    update model

--- myapp/models.py
+++ myapp/models.py
@@ -19,7 +19,6 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     # User
-
     posts = db.relationship('ThriftList', backref='author', lazy=True)
 
     def __init__(self, email, username, password):
@@ -34,21 +33,20 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Username {self.username}"
 
-class ThriftList(db.Model):
-    __tablename__ = 'thrift_lists'
+class ThriftItem(db.Model):
+    __tablename__ = 'thrift_items'
+
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    title = db.Column(db.String(140), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    # thriftList
-
+    name = db.Column(db.String(140), nullable=False)
+    item_details = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
-    def __init__(self, title, text, user_id):
-        self.title = title
-        self.text = text
+    def __init__(self, name, item_details, user_id):
+        self.name = name
+        self.item_details = item_details
         self.user_id = user_id
     
     def __repr__(self):
-        return f"Post ID: {self.id} -- Date: {self.date} --- Title: {self.Title}"
+        return f"ThriftItem ID: {self.id} -- Date: {self.date} --- Name: {self.Name}"
