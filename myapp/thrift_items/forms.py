8e2d7b6cbfc923594684362611b9fc99f95408ad
AJKAR <karczewskiamanda@gmail.com>
2022-04-14 23:39:07 -0500

    add conditional buttons, style register/login

--- myapp/thrift_items/forms.py
+++ myapp/thrift_items/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 class ThriftItemForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    item_details = TextAreaField('ItemDetails', validators=[DataRequired()])
+    item_details = TextAreaField('Details', validators=[DataRequired()])
     submit = SubmitField('POST >')
